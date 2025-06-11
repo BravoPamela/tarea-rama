914a656643d777405b0fa62f6fde5a19e31d672b
BravoPamela <tklpame11@gmail.com>
2025-06-10 23:42:39 -0500

    tarea de colores

---
+++ src/main/java/com/mycompany/colores/Colores.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.colores;
+
+/**
+ *
+ * @author User
+ */
+public class Colores {
+
+    public static void main(String[] args) {
+        System.out.println("TE AMO MI VIDA");
+        System.out.println("Disculapa si te hable feo, estaba estresada");
+        System.out.println("gracias, te amo muchooooo");
+    }
+}
